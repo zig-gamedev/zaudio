bc37245a1a0b8a361f7c29c43d140744eb875b31
Senryoku <maretverdant@gmail.com>
2025-08-21 03:48:48 +0200

    Compiles with zig 0.15.1

--- build.zig
+++ build.zig
@@ -8,31 +8,36 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/zaudio.zig"),
     });
 
-    const miniaudio = b.addStaticLibrary(.{
-        .name = "miniaudio",
+    const miniaudio = b.addModule("miniaudio", .{
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(miniaudio);
+    const miniaudio_lib = b.addLibrary(.{
+        .name = "miniaudio",
+        .root_module = miniaudio,
+        .linkage = .static,
+    });
+
+    b.installArtifact(miniaudio_lib);
 
     miniaudio.addIncludePath(b.path("libs/miniaudio"));
-    miniaudio.linkLibC();
+    miniaudio_lib.linkLibC();
 
     if (target.result.os.tag == .macos) {
         if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
-            miniaudio.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
-            miniaudio.addSystemIncludePath(system_sdk.path("macos12/usr/include"));
-            miniaudio.addLibraryPath(system_sdk.path("macos12/usr/lib"));
+            miniaudio_lib.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
+            miniaudio_lib.addSystemIncludePath(system_sdk.path("macos12/usr/include"));
+            miniaudio_lib.addLibraryPath(system_sdk.path("macos12/usr/lib"));
         }
-        miniaudio.linkFramework("CoreAudio");
-        miniaudio.linkFramework("CoreFoundation");
-        miniaudio.linkFramework("AudioUnit");
-        miniaudio.linkFramework("AudioToolbox");
+        miniaudio_lib.linkFramework("CoreAudio");
+        miniaudio_lib.linkFramework("CoreFoundation");
+        miniaudio_lib.linkFramework("AudioUnit");
+        miniaudio_lib.linkFramework("AudioToolbox");
     } else if (target.result.os.tag == .linux) {
-        miniaudio.linkSystemLibrary("pthread");
-        miniaudio.linkSystemLibrary("m");
-        miniaudio.linkSystemLibrary("dl");
+        miniaudio_lib.linkSystemLibrary("pthread");
+        miniaudio_lib.linkSystemLibrary("m");
+        miniaudio_lib.linkSystemLibrary("dl");
     }
 
     miniaudio.addCSourceFile(.{
@@ -56,15 +61,19 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run zaudio tests");
 
-    const tests = b.addTest(.{
-        .name = "zaudio-tests",
+    const test_module = b.addModule("test", .{
         .root_source_file = b.path("src/zaudio.zig"),
         .target = target,
         .optimize = optimize,
     });
+
+    const tests = b.addTest(.{
+        .name = "zaudio-tests",
+        .root_module = test_module,
+    });
     b.installArtifact(tests);
 
-    tests.linkLibrary(miniaudio);
+    tests.linkLibrary(miniaudio_lib);
 
     test_step.dependOn(&b.addRunArtifact(tests).step);
 }
