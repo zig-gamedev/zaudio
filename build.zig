b9997fc34640d9c80e25b7829b9ad53db8bcf7f2
Alex Boehm <kavsuy4jw1qyqzfv2524jyt6@systemli.org>
2026-07-11 05:40:19 +0930

    Update build for Zig 0.16 (#18)

--- build.zig
+++ build.zig
@@ -22,22 +22,22 @@ pub fn build(b: *std.Build) void {
     b.installArtifact(miniaudio_lib);
 
     miniaudio.addIncludePath(b.path("libs/miniaudio"));
-    miniaudio_lib.linkLibC();
+    miniaudio.linkSystemLibrary("c", .{});
 
     if (target.result.os.tag == .macos) {
         if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
-            miniaudio_lib.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
-            miniaudio_lib.addSystemIncludePath(system_sdk.path("macos12/usr/include"));
-            miniaudio_lib.addLibraryPath(system_sdk.path("macos12/usr/lib"));
+            miniaudio.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
+            miniaudio.addSystemIncludePath(system_sdk.path("macos12/usr/include"));
+            miniaudio.addLibraryPath(system_sdk.path("macos12/usr/lib"));
         }
-        miniaudio_lib.linkFramework("CoreAudio");
-        miniaudio_lib.linkFramework("CoreFoundation");
-        miniaudio_lib.linkFramework("AudioUnit");
-        miniaudio_lib.linkFramework("AudioToolbox");
+        miniaudio.linkFramework("CoreAudio", .{});
+        miniaudio.linkFramework("CoreFoundation", .{});
+        miniaudio.linkFramework("AudioUnit", .{});
+        miniaudio.linkFramework("AudioToolbox", .{});
     } else if (target.result.os.tag == .linux) {
-        miniaudio_lib.linkSystemLibrary("pthread");
-        miniaudio_lib.linkSystemLibrary("m");
-        miniaudio_lib.linkSystemLibrary("dl");
+        miniaudio.linkSystemLibrary("pthread", .{});
+        miniaudio.linkSystemLibrary("m", .{});
+        miniaudio.linkSystemLibrary("dl", .{});
     }
 
     miniaudio.addCSourceFile(.{
@@ -75,7 +75,7 @@ pub fn build(b: *std.Build) void {
     });
     b.installArtifact(tests);
 
-    tests.linkLibrary(miniaudio_lib);
+    tests.root_module.linkLibrary(miniaudio_lib);
 
     test_step.dependOn(&b.addRunArtifact(tests).step);
 }
--- src/zaudio.zig
+++ src/zaudio.zig
@@ -3157,14 +3157,14 @@ pub const Fence = opaque {
 //--------------------------------------------------------------------------------------------------
 var mem_allocator: ?std.mem.Allocator = null;
 var mem_allocations: ?std.AutoHashMap(usize, usize) = null;
-var mem_mutex: std.Thread.Mutex = .{};
+var mem_mutex: std.Io.Mutex = .init;
 const mem_alignment = 16;
 
 extern var zaudioMallocPtr: ?*const fn (size: usize, _: ?*anyopaque) callconv(.c) ?*anyopaque;
 
 fn zaudioMalloc(size: usize, _: ?*anyopaque) callconv(.c) ?*anyopaque {
-    mem_mutex.lock();
-    defer mem_mutex.unlock();
+    std.Io.Threaded.mutexLock(&mem_mutex);
+    defer std.Io.Threaded.mutexUnlock(&mem_mutex);
 
     const zig_version = @import("builtin").zig_version;
 
@@ -3187,8 +3187,8 @@ fn zaudioMalloc(size: usize, _: ?*anyopaque) callconv(.c) ?*anyopaque {
 extern var zaudioReallocPtr: ?*const fn (ptr: ?*anyopaque, size: usize, _: ?*anyopaque) callconv(.c) ?*anyopaque;
 
 fn zaudioRealloc(ptr: ?*anyopaque, size: usize, _: ?*anyopaque) callconv(.c) ?*anyopaque {
-    mem_mutex.lock();
-    defer mem_mutex.unlock();
+    std.Io.Threaded.mutexLock(&mem_mutex);
+    defer std.Io.Threaded.mutexUnlock(&mem_mutex);
 
     const old_size = if (ptr != null) mem_allocations.?.get(@intFromPtr(ptr.?)).? else 0;
     const old_mem = if (old_size > 0)
@@ -3212,8 +3212,8 @@ extern var zaudioFreePtr: ?*const fn (maybe_ptr: ?*anyopaque, _: ?*anyopaque) ca
 
 fn zaudioFree(maybe_ptr: ?*anyopaque, _: ?*anyopaque) callconv(.c) void {
     if (maybe_ptr) |ptr| {
-        mem_mutex.lock();
-        defer mem_mutex.unlock();
+        std.Io.Threaded.mutexLock(&mem_mutex);
+        defer std.Io.Threaded.mutexUnlock(&mem_mutex);
 
         const size = mem_allocations.?.fetchRemove(@intFromPtr(ptr)).?.value;
         const mem = @as([*]align(mem_alignment) u8, @ptrCast(@alignCast(ptr)))[0..size];
@@ -3317,7 +3317,7 @@ test "zaudio.soundgroup.basic" {
 }
 
 test {
-    std.testing.refAllDeclsRecursive(@This());
+    std.testing.refAllDecls(@This());
 }
 
 test "zaudio.fence.basic" {
@@ -3452,7 +3452,7 @@ test "zaudio.audio_buffer" {
     sound.setLooping(true);
     try sound.start();
 
-    std.Thread.sleep(1e8);
+    try std.testing.io.sleep(.fromNanoseconds(1e8), .awake);
 }
 
 test "zaudio.data_converter" {
